ce22f79a8ad353fb2c540b93585f0603e856bd66
tony.chenjy <tony.chenjy@foxmail.com>
2019-02-03 12:23:15 +0800

    c

--- src/main/java/sentiment_analysis/Main.java
+++ src/main/java/sentiment_analysis/Main.java
@@ -1,11 +1,41 @@
 package sentiment_analysis;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
 /**
  * @author cm
  * @date 2019/2/3 0003 10:03
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration conf = new Configuration();
+        conf.set("dictionary", args[2]);
 
+        Job job = Job.getInstance(conf);
+
+        job.setJarByClass(Main.class);
+
+        job.setMapperClass(SentimentMapper.class);
+        job.setReducerClass(SentimentReducer.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.waitForCompletion(true);
     }
 }
