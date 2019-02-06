eeba8453cb5db1f414b9a19e9f53793bf844cc99
tony-chenjy <tony.chenjy@foxmail.com>
2019-02-07 00:25:50 +0800

    c

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -6,8 +6,8 @@ package test;
  */
 public class Main {
     public static void main(String[] args) {
-        String line = "This is   the content of document1";
-        String[] words = line.split(" +");
+        String line = "This is   the content of document1 ";
+        String[] words = line.split("\\s+");
 
         System.out.println(words.length); // TODO
         for (String word : words) {
