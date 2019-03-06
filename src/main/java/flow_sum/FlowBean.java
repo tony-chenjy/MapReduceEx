7275c2ee07003a30289314f9deacd25587b4edff
tony.chenjy <tony.chenjy@foxmail.com>
2019-03-07 07:18:28 +0800

    upload mapred

---
+++ src/main/java/flow_sum/FlowBean.java
@@ -0,0 +1,68 @@
+package flow_sum;
+
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * @author tony.chenjy
+ * @date 2019-03-07
+ */
+public class FlowBean implements Writable {
+
+    private long download_sum;
+    private long upload_sum;
+    private long total_sum;
+
+    public FlowBean() {}
+
+    public FlowBean(long download_sum, long upload_sum) {
+        this.setFlow(download_sum, upload_sum);
+    }
+
+    public void setFlow(long download_sum, long upload_sum) {
+        this.download_sum = download_sum;
+        this.upload_sum = upload_sum;
+        this.total_sum = this.download_sum + this.upload_sum;
+    }
+
+    public long getDownload_sum() {
+        return download_sum;
+    }
+
+    public void setDownload_sum(long download_sum) {
+        this.download_sum = download_sum;
+    }
+
+    public long getUpload_sum() {
+        return upload_sum;
+    }
+
+    public void setUpload_sum(long upload_sum) {
+        this.upload_sum = upload_sum;
+    }
+
+    public long getTotal_sum() {
+        return total_sum;
+    }
+
+    public void setTotal_sum(long total_sum) {
+        this.total_sum = total_sum;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeLong(download_sum);
+        dataOutput.writeLong(upload_sum);
+        dataOutput.writeLong(total_sum);
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        this.download_sum = dataInput.readLong();
+        this.upload_sum = dataInput.readLong();
+        this.total_sum = dataInput.readLong();
+    }
+}
---
+++ src/main/java/flow_sum/FlowSumMapReduce.java
@@ -0,0 +1,82 @@
+package flow_sum;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+/**
+ * @author tony.chenjy
+ * @date 2019-02-28
+ */
+public class FlowSumMapReduce {
+
+    public static class FlowSumMapper extends Mapper<LongWritable, Text, Text, FlowBean> {
+        Text outputKey = new Text();
+        FlowBean outputValue = new FlowBean();
+
+        protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, FlowBean>.Context context) throws IOException, InterruptedException {
+            // inputValue: 13726230503	00-FD-07-A4-72-B8:CMCC	120.196.100.82	i02.c.aliimg.com	24	27	2481	24681	200
+            // outputKey: phone_number
+            // outputValue: download_flow\tupload_flow
+
+            String[] fields = value.toString().trim().split("\t");
+
+            String phone_number = fields[0];
+            String download_flow = fields[fields.length - 3];
+            String upload_flow = fields[fields.length - 2];
+
+            outputKey.set(phone_number);
+            outputValue.setFlow(Long.valueOf(download_flow), Long.valueOf(upload_flow));
+            context.write(outputKey, outputValue);
+        }
+    }
+
+    public static class FlowSumReducer extends Reducer<Text, FlowBean, Text, FlowBean> {
+        FlowBean outputValue = new FlowBean();
+
+        protected void reduce(Text key, Iterable<FlowBean> values, Reducer<Text, FlowBean, Text, FlowBean>.Context context) throws IOException, InterruptedException {
+            // inputKey: phone_number
+            // inputValue: download_flow\tupload_flow
+            // outputKey: phone_number
+            // outputValue: download_flow\tupload_flow\ttotal_flow
+
+            long upload_flow = 0, download_flow = 0, total_flow = 0;
+            for (FlowBean value : values) {
+                download_flow += value.getDownload_sum();
+                upload_flow += value.getUpload_sum();
+            }
+            outputValue.setFlow(download_flow, upload_flow);
+            context.write(key, outputValue);
+        }
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        String inputPath = "src/main/resources/flow_sum/input";
+        String outputPath = "src/main/resources/flow_sum/output";
+
+        Configuration conf = new Configuration();
+
+        Job job = Job.getInstance(conf);
+
+        job.setJarByClass(FlowSumMapReduce.class);
+
+        job.setMapperClass(FlowSumMapReduce.FlowSumMapper.class);
+        job.setReducerClass(FlowSumMapReduce.FlowSumReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(FlowBean.class);
+
+        FileInputFormat.setInputPaths(job, inputPath);
+        FileOutputFormat.setOutputPath(job, new Path(outputPath));
+
+        job.waitForCompletion(true);
+    }
+}
